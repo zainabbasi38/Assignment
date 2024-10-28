773e76d38469c0aa1d5be2d6d78c051f9572ff38
zainabbasi38 <zainabbasi7534@gmail.com>
2024-10-27 23:34:59 -0700

    Add code

---
+++ 05_triangle_perimeter.py
@@ -0,0 +1,14 @@
+def main():
+# Get the three side length of triangle
+    side1 : float = float(input("Enetr length of first side: "))
+    side2 : float = float(input("Enetr length of second side: "))
+    side3 : float = float(input("Enetr length of third side: "))
+
+# print out all sides and sum
+    print(f"the perimeter of triangle is  {side1 + side2 + side3}")
+
+main()
+
+if __name__ == "__main__":
+    main()
+    
