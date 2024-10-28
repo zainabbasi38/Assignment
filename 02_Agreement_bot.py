23d34c728ffc589c6da0c03f6e087a9e5fdb054d
zainabbasi38 <zainabbasi7534@gmail.com>
2024-10-27 20:39:04 -0700

    Add code

---
+++ 02_Agreement_bot.py
@@ -0,0 +1,10 @@
+def main():
+    print("what's your favourite animal? ")
+    user_resonse = input("My favourite animal is ")
+    print(f"Yes, my also favourite animal is {user_resonse}")
+    print()
+
+main()
+
+if __name__ == "__main__":
+    main()
