bb780f014c97ec3e8f68a9b4a5c59bc1ef370937
zainabbasi38 <zainabbasi7534@gmail.com>
2024-10-27 20:25:05 -0700

    Add code

---
+++ 01_add_two_numbers.py
@@ -0,0 +1,8 @@
+def add_mumbers():
+    print("This program performs add two numbers")
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    result = num1 + num2
+    print(f"Total: {result}")
+
+add_mumbers()
