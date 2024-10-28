29772be36bc2cd1a743c11eea2d463207d8e987c
zainabbasi38 <zainabbasi7534@gmail.com>
2024-10-27 23:03:36 -0700

    Add code

---
+++ 04_how_old_are_they.py
@@ -0,0 +1,16 @@
+def main():
+    Anton : int = 21
+    Beth : int = 6 + Anton
+    Chen : int = 20 + Beth
+    Drew : int = Chen + Anton
+    Ethan : int = Chen
+
+# print out all the ages
+    print("Anton is " + str(Anton))
+    print("Beth is " + str(Beth)) 
+    print("Chen is " + str(Chen)) 
+    print("Drew is " + str(Drew)) 
+    print("Ethan is " + str(Ethan))
+    print()
+main()
+
