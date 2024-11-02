ae697cee7d51056851da9298e2a9b3d6aaeb0972
zainabbasi38 <zainabbasi7534@gmail.com>
2024-11-01 23:48:06 -0700

    Add block

--- 20_get_list.py
+++ 20_get_list.py
@@ -1,3 +1,10 @@
+"""
+
+Problem:
+Write a program which continuously asks the user to enter values which are added one by one into a list.
+ When the user presses enter without typing anything, print the list
+
+"""
 def main():
     list = []
 
