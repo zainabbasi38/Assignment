27f35f4bffbc109b029b7bc6ff82709216e11b77
zainabbasi38 <zainabbasi7534@gmail.com>
2024-11-01 23:27:31 -0700

    code

--- 19_get_last_element.py
+++ 19_get_last_element.py
@@ -1,3 +1,11 @@
+"""
+
+Problem:
+Fill out the function get_last_element(lst) which takes in a list lst as a parameter
+ and prints the last element in the list. The list is guaranteed to be non-empty, but 
+ there are no guarantees on its length.
+
+"""
 def get_last_element(Ist):
 
     print(Ist[-1])
