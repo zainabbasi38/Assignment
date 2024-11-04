61371c24376b850af1027c481f6cb41c6d564e55
zainabbasi38 <zainabbasi7534@gmail.com>
2024-11-03 23:31:44 -0800

    Add

---
+++ 28_phonebook.py
@@ -0,0 +1,34 @@
+def read_phone_numbers():
+    phonebook = {}
+
+    while True:
+        name = input("Name: ")
+        if name == "":
+            break
+        number = input("Number: ")
+        phonebook[name] = number
+    return phonebook
+
+def print_phonebook(phonebook):
+    for name in phonebook:
+        print(str(name) + " -> "+ str(phonebook[name]))
+
+def lockup(phonebook):
+    while True:
+       name = input("Enter name to lockup: ")
+
+       if name == "":
+            break
+       if name not in phonebook:
+            print(name + "not in the phonebook")
+
+       else:
+            print(phonebook[name])
+    
+def main():
+    book = read_phone_numbers()
+    print_phonebook(book)
+    lockup(book)
+
+if __name__=="__main__":
+    main()
