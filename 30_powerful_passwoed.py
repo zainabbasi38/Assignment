a45e949b81763233571dfcc3bf1c2fcc3925b057
zainabbasi38 <zainabbasi7534@gmail.com>
2024-11-04 01:46:17 -0800

    Add

---
+++ 30_powerful_passwoed.py
@@ -0,0 +1,36 @@
+from hashlib import sha256
+
+def login(email,stored_logins,password_to_check):
+
+    if stored_logins[email] == hash_password(password_to_check):
+        return True
+
+    return False
+
+def hash_password(password):
+
+    return sha256(password.encode()).hexdigest()
+
+def main():
+    
+    stored_logins = {
+        "example@gmail.com": "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",
+        "code_in_placer@cip.org": "973607a4ae7b4cf7d96a100b0fb07e8519cc4f70441d41214a9f811577bb06cc",
+        "student@stanford.edu": "882c6df720fd99f5eebb1581a1cf975625cea8a160283011c0b9512bb56c95fb"
+
+    }
+
+    print(login("example@gmail.com", stored_logins, "word" ))
+    print(login("example@gmail.com", stored_logins, "password"))
+
+    print(login("code_in_placer@cip.org", stored_logins, "Karel"))
+    print(login("code_in_placer@cip.org", stored_logins, "karel"))
+
+    print(login("student@stanford.edu", stored_logins, "password"))
+    print(login("student@stanford.edu", stored_logins, "123!@##"))
+
+
+if __name__=="__main__":
+    main()
+
+          
