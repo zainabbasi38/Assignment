a233b5623a1a60f1301ce121699b2773d2bfeeb5
zainabbasi38 <zainabbasi7534@gmail.com>
2024-11-05 23:06:23 -0800

    Add

---
+++ 37_Average.py
@@ -0,0 +1,14 @@
+def average(a:float, b:float):
+    sum = a + b
+    return sum / 2
+
+def main():
+    avg1 = average(10,4)
+    avg2 = average(33,99)
+
+    print("Avg1 ",avg1)
+    print("Avg2 ",avg2)
+    print("Final ")
+
+if __name__=="__main__":
+    main()
